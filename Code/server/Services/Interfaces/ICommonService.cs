647d8ba1af96b7b628d0663dde1b78670a87a638
vaishak-11 <vaishaksathish697@gmail.com>
2024-07-02 22:34:11 +0530

    removed unwanted files

--- Code/server/Services/Interfaces/ICommonService.cs
+++
@@ -1,9 +0,0 @@
-﻿using RecommendationEngineServer.Models.DTOs;
-
-namespace RecommendationEngineServer.Services.Interfaces
-{
-    public interface ICommonService
-    {
-        ServerResponse CreateResponse(string name, Object value);
-    }
-}
